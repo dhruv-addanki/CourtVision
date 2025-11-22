028387e07a4c097660bcb3846f833287e2d4888e
dhruv-addanki <dhruvaddanki@gmail.com>
2025-11-22 14:42:43 -0500

    Initial Commit

---
+++ CourtVision/CourtVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtVisionApp.swift
+//  CourtVision
+//
+//  Created by Dhruv .A on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CourtVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
